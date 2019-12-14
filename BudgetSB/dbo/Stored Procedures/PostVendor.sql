a1fb2a18166f14e76429a791257be35903bfd09c
Elizabeth Noble <elizabeth.a.noble@outlook.com>
2019-12-14 09:39:50 -0500

    Create Budget database

---
+++ BudgetSB/dbo/Stored Procedures/PostVendor.sql
@@ -0,0 +1,14 @@
+﻿
+
+CREATE   PROCEDURE dbo.PostVendor
+	@VendorName		VARCHAR(100),
+	@Address1		VARCHAR(100),
+	@City			VARCHAR(100),
+	@StateName		VARCHAR(100),
+	@ZipCode		VARCHAR(100),
+	@PaymentDate	INT
+AS
+
+	INSERT INTO dbo.Vendor (VendorName, Address1, City, StateName, ZipCode, PaymentDate)
+	SELECT @VendorName, @Address1, @City, @StateName, @ZipCode, @PaymentDate
+
---
+++ BudgetSB/dbo/Tables/Vendor.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Vendor] (
+    [VendorID]    INT           IDENTITY (1, 1) NOT NULL,
+    [VendorName]  VARCHAR (100) NOT NULL,
+    [Address1]    VARCHAR (100) NOT NULL,
+    [City]        VARCHAR (100) NOT NULL,
+    [StateName]   VARCHAR (100) NOT NULL,
+    [ZipCode]     VARCHAR (100) NOT NULL,
+    [PaymentDate] INT           NOT NULL,
+    CONSTRAINT [pk_VendorID] PRIMARY KEY CLUSTERED ([VendorID] ASC)
+);
+
