cc1ae3814020bf6518c0f061c8838c2e081ddc1c
Elizabeth Noble <elizabeth.a.noble@outlook.com>
2020-02-29 14:07:40 -0500

    Added VendorTransaction

---
+++ BudgetSB/dbo/Tables/VendorTransaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE dbo.VendorTransaction 
+(
+	VendorID		INT									NOT NULL,
+	BudgetAmount	DECIMAL(8,2)						NOT NULL,
+	PaymentAmount	DECIMAL(8,2)						NOT NULL,
+	PaymentDate		DATETIME							NOT NULL,
+	CONSTRAINT fk_VendorTransaction_VendorID FOREIGN KEY (VendorID) REFERENCES Vendor(VendorID)
+);
