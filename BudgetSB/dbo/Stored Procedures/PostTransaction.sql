8d5f0c47a76ba12dd73ca4a57a8a825fdfacc980
Elizabeth Noble <elizabeth.a.noble@outlook.com>
2020-03-05 20:56:10 -0500

    Add another stored procedure

---
+++ BudgetSB/dbo/Stored Procedures/PostTransaction.sql
@@ -0,0 +1,12 @@
+﻿
+
+CREATE   PROCEDURE dbo.PostTransaction
+	@VendorID		INT,
+	@BudgetAmount	DECIMAL(7,2),
+	@PaymentAmount	DECIMAL(7,2),
+	@PaymentDate	DATETIME2
+AS
+
+	INSERT INTO dbo.VendorTransaction(VendorID, BudgetAmount, PaymentAmount, PaymentDate)
+	SELECT @VendorID, @BudgetAmount, @PaymentAmount, @PaymentDate
+
